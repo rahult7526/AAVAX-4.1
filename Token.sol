81b41ec02b5cb9d58930594eb6eacfd583cdf8ab
RAHUL TIWARY <91451569+rahult7526@users.noreply.github.com>
2024-08-21 16:52:13 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,1 +1,10 @@
-zsb
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GameToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("GameToken", "GT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
